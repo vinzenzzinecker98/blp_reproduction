fb29f512cb9877392e8b73b14740109ad7b70c20
vinzenzzinecker98 <vinzenz@zinecker.de>
2022-03-04 19:22:08 +0100

    Further grid search

---
+++ scripts/gridsearch_margin_2e4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /pfs/data5/home/kit/stud/ucgvm//environment/bin/activate
+echo '2e4, margin'
+python train.py link_prediction with \
+dataset='FB15k-237' \
+inductive=True \
+dim=128 \
+model='linearsbert' \
+rel_model='transe' \
+loss_fn='margin' \
+encoder_name='sentence-transformers/bert-base-nli-mean-tokens' \
+regularizer=0 \
+max_len=32 \
+num_negatives=64 \
+lr=2e-4 \
+use_scheduler=True \
+batch_size=64 \
+emb_batch_size=512 \
+eval_batch_size=64 \
+max_epochs=40 \
+checkpoint=None \
+use_cached_text=False \
+output_dir="output/gridsearch/2e4margin/"
